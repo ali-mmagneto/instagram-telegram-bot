6a31d87a2c692367ecdafa6e96a6a2629c172b52
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-02-12 13:27:39 +0300

    Update sample-config.py

--- sample-config.py
+++ sample-config.py
@@ -1,9 +1,9 @@
-TOKEN = 'YOUR-BOT-TOKEN'
-admin_id = 0
+TOKEN = '583168942:AAHgAsyWjSJmdxBzGqM3xizKaba2HJcLDqw'
+admin_id = 1276627253
 helper_id = 0
 
 ##### upload_file.py config #####
-api_id = 0
-api_hash = 'api_hash'
-name = 'name'
-bot_username = 'bot_username'
+api_id = 2374174
+api_hash = 'c23db4aa92da73ff603666812268597a'
+name = 'instabot'
+bot_username = '@leech3Robot'
